195ba2be1ecc07ad316a3f75633b4659071e7400
Chris Wendt <chrismwendt@gmail.com>
2020-01-30 17:24:49 -0700

    expand example

--- example/lib/src/main.dart
+++ example/lib/src/main.dart
@@ -6,4 +6,22 @@ String greetingWords = 'Hello world!';
 void main() {
   var content = querySelector('#content');
   content.text = greetingWords;
+  var config = new Config._(output: "some output", format: OutputFormat.LSIF);
+  print(config);
 }
+
+/** Holds information about a foo. */
+class Config {
+  /** Absolute path to the output directory, comes from --output, defaults to --input */
+  final String output;
+  /** Some format. */
+  final OutputFormat format;
+
+  /** A static string. */
+  static const String DART_SDK = "dart-sdk";
+
+  Config._({this.output, this.format});
+}
+
+/** JSON, HTML, GitHub, LSIF. */
+enum OutputFormat { JSON, HTML, GITHUB, LSIF }
